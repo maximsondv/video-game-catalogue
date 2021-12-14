153d74732811763cda94746499fbacf782fff5d3
Maxim Son <maximsondv@gmail.com>
2021-12-14 22:24:30 +0600

    Interceptor,services setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FormsModule} from "@angular/forms";
-import {HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {GaugeModule} from "angular-gauge";
 import {MatTabsModule} from "@angular/material/tabs";
 import {MatIconModule} from "@angular/material/icon";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatSelectModule} from "@angular/material/select";
-import { SearchBarComponent } from './components/search-bar/search-bar.component';
-import { AppRoutingModule } from './app-routing.module';
-import {RouterModule} from "@angular/router";
+import {SearchBarComponent} from './components/search-bar/search-bar.component';
+import {AppRoutingModule} from './app-routing.module';
+import {HomeComponent} from './components/home/home.component';
+import {HttpHeadersInterceptor} from "./interceptors/http-headers.interceptor";
+import {HttpErrorsInterceptor} from "./interceptors/http-errors.interceptor";
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchBarComponent
+    SearchBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -29,10 +32,20 @@ import {RouterModule} from "@angular/router";
     MatIconModule,
     MatFormFieldModule,
     MatSelectModule,
-    AppRoutingModule,
-    RouterModule
+    AppRoutingModule],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpHeadersInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpErrorsInterceptor,
+      multi: true
+    }
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
